c9699f3dbc9298a32af668b9864097ab468326ea
CrveniLuigi <130222560+CrveniLuigi@users.noreply.github.com>
2023-07-21 20:56:38 +0200

    started on js

---
+++ images/app.js
@@ -0,0 +1,6 @@
+const planetPic = document.querySelector(".planet")
+
+const btnInternal = document.querySelector(".btn-internal")
+btnInternal.addEventListener('click', () => {
+    planetPic.src = '.images/geology-earth.png'
+})
