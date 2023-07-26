abcac3de37a11867dff92deeb4f9684485b482b5
CrveniLuigi <130222560+CrveniLuigi@users.noreply.github.com>
2023-07-26 21:12:27 +0200

    finished earth.html

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 const planetPic = document.querySelector(".image")
+const paragraph = document.querySelector(".paragraph")
 
-const btn1 = document.querySelector(".btn-internal");
+const btn1 = document.querySelector(".btn-overview");
 btn1.addEventListener('click', () => {
-    planetPic.src = "images/planet-earth-internal.svg"
+    planetPic.src = "images/planet-earth.svg"
+    paragraph.innerText = "Third planet from the Sun and the only known planet to harbor life. About 29.2% of Earth's surface is land with remaining 70.8% is covered with water.Earth's distance from the Sun, physical properties and geological history have allowed life to evolve and thrive."
+
 })
 
-const btn2 = document.querySelector(".btn-overview");
+const btn2 = document.querySelector(".btn-internal");
 btn2.addEventListener('click', () => {
-    planetPic.src = "images/planet-earth.svg"
+    planetPic.src = "images/planet-earth-internal.svg"
+    paragraph.innerText = "Earth's interior, like that of the other terrestrial planets, is divided into layers by their chemical or physical (rheological) properties. The outer layer is a chemically distinct silicate solid crust, which is underlain by a highly viscous solid mantle."
+
 })
+
+
 
 const btn3 = document.querySelector(".btn-surface");
 btn3.addEventListener('click', () => {
-    planetPic.src = "images/geology-earth.png"
+    planetPic.src = "images/geology-earth.png";
+    planetPic.setAttribute("width", 450);
+    planetPic.setAttribute("height", 450);
+    paragraph.innerText = "The total surface area of Earth is about 510 million km2. The continental crust consists of lower density material such as the igneous rocks granite and andesite. Less common is basalt, a denser volcanic rock that is the primary constituent of the ocean floors."
+
+
 })
+
+
+
